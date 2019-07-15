e3cc7e9465ad92f61dac2b0c31c3d82733fb4814
sbirdsill <43529733+sbirdsill@users.noreply.github.com>
2019-07-14 21:03:28 -0700

    Update Backup-MSSQL-Databases.ps1

--- Backup-MSSQL-Databases.ps1
+++ Backup-MSSQL-Databases.ps1
@@ -1,6 +1,6 @@
-﻿# Backup-MSSQL-Databases.ps1
-# Version 1.2
-# Last updated 11/5/2018
+# Backup-MSSQL-Databases.ps1
+# Version 1.3
+# Last updated Jul-14-2019
 #
 # This PowerShell script will perform a full backup of all defined MSSQL databases to a BAK file.
 # Use Task Scheduler to have this script run daily. The account you run this under will require the proper permission to perform backups in SQL server.
@@ -11,19 +11,23 @@
 #
 # IMPORTANT: Parts that will need be modified for your environment are double commented (##). The rest can safely be left alone.
 
+## Set SQL Server instance.
+$sqlserver = "SQLServerName\SQLInstanceName"
+
+## Set the database name(s)
+$Databases = @("Database1", "Database2", "Database3")
+
+## Set backup path
+$TopLevelBackupPath = "E:\MSSQLBackups"
+
 # Define date parameters.
 $timestamp = Get-Date -Format yyyy-MM-dd
 
-## Set SQL Server\SQL Database.
-$sqldatabase = "SQLServerName\SQLInstanceName"
-
-## Set backup path
-$TopLevelBackupPath = "E:\Backups"
-
-# Set GetDate var to Get-Date command
-$GetDate = Get-Date
+# Get the current date
+$Date = Get-Date
 
 # Create backup folders and Readme file if they don't already exist
+
 if (-not (Test-Path "$TopLevelBackupPath\Daily"))
 {
   New-Item -Path $TopLevelBackupPath -Name "Daily" -ItemType "directory"
@@ -41,37 +45,34 @@ if (-not (Test-Path "$TopLevelBackupPath\Readme.txt"))
   New-Item -Path $TopLevelBackupPath -Name "Readme.txt" -ItemType "file" -Value "SQL backups will be kept in this folder. They are performed with Backup-MSSQL-Databases.ps1"
 }
 
+
 # Function to start the backup and cleanup process. 
 function Start-Backup {
 
+  # Start the log
+  Start-Transcript -Path $BackupPath\MSSQL_Backup_Log_$timestamp.log -Append
+
   # Create backup folder.
-
   Write-Host "[1] Creating backup folder..."
-
   New-Item -Path "$BackupPath" -Name "$timestamp" -ItemType "directory" -Verbose
 
-  ## Start backing up SQL databases. Where DATABASENAME, replace with your actual database name.
+  foreach ($Database in $Databases) {
 
-  Write-Host "[2] Backing up database(s)..."
+    # Start backing up SQL databases.
+    Write-Host "[2] Backing up $Database..."
+    SQLCMD.EXE -E -S $sqlserver -Q "BACKUP DATABASE $Database TO DISK='$BackupPath\$timestamp\$Database`_$timestamp.bak' WITH FORMAT"
 
-  SQLCMD.EXE -E -S $sqldatabase -Q "BACKUP DATABASE DATABASENAME TO DISK='$BackupPath\$timestamp\DATABASENAME_$timestamp.bak' WITH FORMAT"
-
-  ## Verify backed up SQL databases. Where DATABASENAME, replace with your actual database name.
-
-  Write-Host "[3] Verifying backup file(s)..."
-
-  SQLCMD.EXE -E -S $sqldatabase -Q "RESTORE VERIFYONLY FROM DISK = '$BackupPath\$timestamp\DATABASENAME_$timestamp.bak'"
+    # Verify backed up SQL databases.
+    Write-Host "[3] Verifying $Database backup..."
+    SQLCMD.EXE -E -S $sqlserver -Q "RESTORE VERIFYONLY FROM DISK = '$BackupPath\$timestamp\$Database`_$timestamp.bak'"
+  }
 
   # Delete files/folders older than specified time.
-
   Write-Host "[4] Removing old files/folders..."
-
   Get-ChildItem -Path $BackupPath -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force -Verbose
 
   # Delete empty directories.
-
   Get-ChildItem -Path $BackupPath -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse -Verbose
-
   Write-Host "[5] Backup complete."
 }
 
@@ -79,7 +80,6 @@ function Start-Backup {
 function Daily {
   $BackupPath = "$TopLevelBackupPath\Daily"
   $limit = (Get-Date).AddDays(-30)
-  Start-Transcript -Path $BackupPath\MSSQL_Backup_Log_$timestamp.log -Append
   Start-Backup
 }
 
@@ -87,10 +87,8 @@ function Daily {
 function Monthly {
   $Monthly = Get-Date -Day 01
   $limit = (Get-Date).AddDays(-365)
-
-  if (($GetDate.Day -eq $Monthly.Day)) {
+  if (($Date.Day -eq $Monthly.Day)) {
     $BackupPath = "$TopLevelBackupPath\Monthly"
-    Start-Transcript -Path $BackupPath\MSSQL_Backup_Log_$timestamp.log -Append
     Start-Backup
   }
 }
@@ -99,10 +97,8 @@ function Monthly {
 function Yearly {
   $Yearly = Get-Date -Day 01 -Month 01
   $limit = (Get-Date).AddDays(-3650)
-
-  if (($GetDate.Day -eq $Yearly.Day) -and ($GetDate.Month -eq $Yearly.Month)) {
+  if (($Date.Day -eq $Yearly.Day) -and ($Date.Month -eq $Yearly.Month)) {
     $BackupPath = "$TopLevelBackupPath\Yearly"
-    Start-Transcript -Path $BackupPath\MSSQL_Backup_Log_$timestamp.log -Append
     Start-Backup
   }
 }
@@ -111,5 +107,7 @@ Daily
 Monthly
 Yearly
 
-# End of script.
+# End logging and exit
 Write-Host "Exiting..."
+Stop-Transcript
+exit
